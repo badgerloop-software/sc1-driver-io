3fb8321cf848f731619ec75847039ba1e6beaec7
bquan0 <powerbowen@gmail.com>
2022-03-27 12:29:46 -0500

    ads1219 loop function

--- embedded/devices/src/ads1219.cpp
+++ embedded/devices/src/ads1219.cpp
@@ -1,16 +1,49 @@
 #include "ads1219.h"
-
+#define WRITE_CONFIG_REG 0x40 // 0100 00xx
+#define READ_CONFIG_REG 0x20 // 0010 00xx
+#define READ_STATUS_REG 0x24 // 0010 01xx
+#define RESET 0x06 // 0000 011x
+#define RDATA 0x10 // 0001 xxxx
+#define START_CONVERSION 0x08 // 0000 100x
+#define CONVERSION_VOLTAGE 244.14e-9
 
 Ads1219::Ads1219(int bus, int addr) : I2c(bus, addr, O_RDWR){}
 
 void Ads1219::loop() {
     while (this->stop_thread != true) {
+        // get the current channel we are on
+        uint8_t configReg = this->read_from_reg(READ_CONFIG_REG); 
+        uint8_t currentChannel = configReg >> 5;
+        // calculate the next channel
+        if(currentChannel == 6 || currentChannel == 0){
+            currentChannel = 3;
+        }
+        else{
+            currentChannel++;
+        }
+        int channel = currentChannel - 3; // used to access the voltages array
+        // set up the next configReg 
+        configReg = (configReg & 0x1F) | (currentChannel << 5);
+        // switch the channel
+        this->write_data<uint8_t>(WRITE_CONFIG_REG, configReg);
+        // read status register until conversion is done(it has a 1 flag)
+        uint8_t statusReg = 0;
+        while(statusReg == 0){
+            statusReg = this->read_from_reg(READ_STATUS_REG);
+            statusReg = statusReg >> 7; // get the leading bit
+        }
+        // get the conversion result using RDATA
 
+        int32_t conversionResult;
+
+        this->read_bytes_from_reg(RDATA, (uint8_t *)&conversionResult, 3);
+        // do math to convert it to voltage and set the mutex variable 
+        this->voltages.at(channel).setValue((float)(conversionResult >> 8) * CONVERSION_VOLTAGE);
     }
 }
 
-float getVoltage(int chan) {
-    
+float Ads1219::getVoltage(int chan) {
+    return this->voltages.at(chan).getValue();
 }
 
 int Ads1219::begin() {
@@ -18,7 +51,7 @@ int Ads1219::begin() {
 
     rc = this->open_i2c();
     if (rc) return rc;
-
+    this->write_byte(RESET);
     ads_thread = std::thread(loop);
 
     return 0;
