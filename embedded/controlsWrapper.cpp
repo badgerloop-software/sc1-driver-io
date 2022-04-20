3fe514b699c08753f9688059200bf7b0f815f6f5
bquan0 <powerbowen@gmail.com>
2022-04-19 19:06:54 -0500

    test loop working

--- embedded/controlsWrapper.cpp
+++ embedded/controlsWrapper.cpp
@@ -7,6 +7,8 @@ controlsWrapper::controlsWrapper() {
 
 }
 
+// This is the firmware main loop. It's called in a separate thread in DataUnpacker.cpp
+// Put your testing code here!
 void controlsWrapper::startThread() {
     // loop goes here
     while(true) {
