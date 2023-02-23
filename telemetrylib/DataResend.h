9cefbdf8ca1f0b08d93f8dae68f30baf302acab2
Mingcan Li <limingcanjim@gmail.com>
2023-02-23 15:24:10 -0600

    I'm sorry :<
    This reverts commit f9f87ba04b9c9654a237739b0493600a4a8cd19e.

---
+++ telemetrylib/DataResend.h
@@ -0,0 +1,33 @@
+//
+// Created by Mingcan Li on 2/23/23.
+//
+
+#ifndef TELEMETRYLIB_DATARESEND_H
+#define TELEMETRYLIB_DATARESEND_H
+
+
+#include <QObject>
+#include "DTI.h"
+class Dataresend : QObject{
+    Q_OBJECT
+public:
+    void setChannel(DTI *channel);
+    void addToQueue(QByteArray arr);
+    /*
+     * IMPORTANT, tell the class if the telemetry class is connected/disconnected
+     * before giving this class the data
+     */
+    void comStatus(bool state);
+    bool isBusy(){return busy;}
+protected:
+    void resend();
+private:
+    DTI *channel;
+    QQueue<QByteArray> q;
+    QMutex mutex;
+    std::thread* t;
+    std::atomic<bool> comstate, busy;
+};
+
+
+#endif //TELEMETRYLIB_DATARESEND_H
