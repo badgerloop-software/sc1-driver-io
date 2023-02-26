51f24380b3aab619b98ca4821ced26be15bd9bf8
Mingcan Li <57700505+MCLiii@users.noreply.github.com>
2023-02-25 18:59:36 -0600

    added comments to the data resend process

--- telemetrylib/DataResend.cpp
+++ telemetrylib/DataResend.cpp
@@ -2,21 +2,21 @@
 // Created by Mingcan Li on 2/23/23.
 //
 
-#include "Dataresend.h"
+#include "DataResend.h"
 
 
-void Dataresend::setChannel(DTI *channel) {
+void DataResend::setChannel(DTI *channel) {
     this->channel = channel;
 }
 
-void Dataresend::addToQueue(QByteArray arr) {
+void DataResend::addToQueue(QByteArray arr) {
     qDebug() << "Queued msg: "<<q.size()+1<<"\n";
     mutex.lock();
     q.enqueue(arr);
     mutex.unlock();
 }
 
-void Dataresend::comStatus(bool state) {
+void DataResend::comStatus(bool state) {
     comstate = state;
     connect(this, SIGNAL(Dataresend::send), channel, SLOT(&DTI::sendData));
     if(state && !q.empty()) {
@@ -25,20 +25,20 @@ void Dataresend::comStatus(bool state) {
     }
 }
 
-void Dataresend::resend() {
-    busy = true;
-    mutex.lock();
+void DataResend::resend() {
+    busy = true; //if dataresend is currently busy, telemetrylib would not send data directly thru channel, and indead send it thru this class
+    mutex.lock(); //use a mutex to stop queue access during resend
     qDebug()<<"sending";
     for(QByteArray& arr: q){
         qDebug()<<".";
         if (!comstate){
             break;
         }
-        mutex.unlock();
+        mutex.unlock(); //additional data can be added during this temporary unlock
         mutex.lock();
         emit(send(arr));
     }
     qDebug()<<"\n";
-    mutex.unlock();
+    mutex.unlock(); //exit the loop data can be added
     busy = false;
 }
