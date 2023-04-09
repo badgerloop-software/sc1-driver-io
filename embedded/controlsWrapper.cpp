875756c77dfe8c5f5a89a00319847e0c05e5043e
bquan0 <powerbowen@gmail.com>
2023-04-09 13:29:21 -0500

    change us to ms, get offsets and set mainIO_heartbeat and mcu_check correctly

--- embedded/controlsWrapper.cpp
+++ embedded/controlsWrapper.cpp
@@ -10,14 +10,14 @@
 #include <embedded/devices/src/ads1219.cpp>
 #include <embedded/drivers/include/serial.h>
 #include <embedded/drivers/src/serial.cpp>
-#define T_MESSAGE_US 70000  // around 1/15 of a second
-#define HEARTBEAT 2         // error state if this # messages that aren't read
+#define T_MESSAGE_MS 1000  // 1 second 
+#define HEARTBEAT 2         // go to error state if this # messages that aren't read
 
 Serial serial;
 QMutex uartMutex;
 
-#define TOTAL_BYTES 399
-controlsWrapper::controlsWrapper(QByteArray &bytes, QMutex &mutex, std::atomic<bool> &restart_enable, QObject *parent, int mainIO_heartbeat_offset, int mcu_check_offset) : QObject(parent), bytes(bytes), mutex(mutex), restart_enable(restart_enable), mainIO_heartbeat_offset(mainIO_heartbeat_offset), mcu_check_offset(mcu_check_offset) {
+#define TOTAL_BYTES 407
+controlsWrapper::controlsWrapper(QByteArray &bytes, QMutex &mutex, std::atomic<bool> &restart_enable, int mainIO_heartbeat_offset, int mcu_check_offset, QObject *parent) : QObject(parent), bytes(bytes), mutex(mutex), restart_enable(restart_enable), mainIO_heartbeat_offset(mainIO_heartbeat_offset), mcu_check_offset(mcu_check_offset) {
     //this->bytes = bytes;
     serial = Serial();
     serial.openDevice(0, 115200);
@@ -28,27 +28,34 @@ controlsWrapper::controlsWrapper(QByteArray &bytes, QMutex &mutex, std::atomic<b
 void controlsWrapper::startThread() {
     int messages_not_received = 0;
     while(true) {
+        std::cout << "===========================================" << std::endl;
         char buffTemp[TOTAL_BYTES];
         // read
         uartMutex.lock();
         // check if we actually read a message 
-        if (serial.readBytes(buffTemp, TOTAL_BYTES, T_MESSAGE_US, 0) == 0) {
-            if (++messages_not_received == HEARTBEAT) {
+        int numBytesRead = serial.readBytes(buffTemp, TOTAL_BYTES, T_MESSAGE_MS, 0); 
+        if (numBytesRead == 0) {
+            std::cout<< "message not received" << std::endl;
+            if (++messages_not_received >= HEARTBEAT) {
+                std::cout << "heartbeat lost" << std::endl;
                 restart_enable = 1;
                 mutex.lock();
-                bytes[mainIO_heartbeat_offset] = 0;
-                bytes[mcu_check_offset] = 1;
-                mutex.lock();
+                // TODO Take a look at backendprocesses.cpp where we set tstamp_hr/mn/sc/ms for setting specific bytes in the byte array
+                buffTemp[mainIO_heartbeat_offset] = 0;
+                buffTemp[mcu_check_offset] = 1;
+                mutex.unlock();
             }
         } else {
             messages_not_received = 0;
+            //restart_enable = 0;
         }
         uartMutex.unlock();
-
+        
         // write restart_enable signal
         uartMutex.lock();
         std::cout << "restart_enable: " << restart_enable << std::endl;
-        serial.writeBytes(&restart_enable, 1); 
+        int write = serial.writeBytes(&restart_enable, 1); 
+        std::cout << "bytes written: " << write << std::endl;
         uartMutex.unlock();
         
         // copy data in char array to QByteArray
@@ -57,11 +64,6 @@ void controlsWrapper::startThread() {
         bytes = QByteArray::fromRawData(buffTemp, TOTAL_BYTES);
         mutex.unlock();
         //usleep(1000000);
-        /*std::cout << "speed: " << dfread.speed << std::endl;
-        std::cout << "linear_accel_x: " << dfread.linear_accel_x << std::endl;
-        std::cout << "cell_group1_voltage: " << dfread.cell_group1_voltage << std::endl;
-        std::cout << "cell_group2_voltage: " << dfread.cell_group2_voltage << std::endl;
-        std::cout << "cell_group3_voltage: " << dfread.cell_group3_voltage << std::endl;*/
         //std::cout << "==========================================" << std::endl;
         sleep(1);
     }
--- embedded/controlsWrapper.h
+++ embedded/controlsWrapper.h
@@ -9,13 +9,15 @@ class controlsWrapper : public QObject
 {
     Q_OBJECT
     public:
-        explicit controlsWrapper(QByteArray &bytes, QMutex &mutex, std::atomic<bool> &restart_enable, QObject *parent = nullptr, int mcu_check_offset, int mainIO_heartbeat_offset);
+        explicit controlsWrapper(QByteArray &bytes, QMutex &mutex, std::atomic<bool> &restart_enable, int mainIO_heartbeat_offset, int mcu_check_offset, QObject *parent = nullptr);
     public slots:
         void startThread();
         // need other slot for a signal
     private:
         QByteArray &bytes;
         QMutex &mutex;
+        int mainIO_heartbeat_offset;
+        int mcu_check_offset;
         std::atomic<bool> &restart_enable;
 };
 
