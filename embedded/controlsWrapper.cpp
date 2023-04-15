aba73cc42b2cf8a17782d931be3ec68fd48b76f1
bquan0 <powerbowen@gmail.com>
2023-04-14 20:42:11 -0500

    lights code using TCA

--- embedded/controlsWrapper.cpp
+++ embedded/controlsWrapper.cpp
@@ -8,6 +8,8 @@
 
 #include <embedded/devices/include/ads1219.h>
 #include <embedded/devices/src/ads1219.cpp>
+#include <embedded/devices/include/tca6416.h>
+#include <embedded/devices/src/tca6416.cpp>
 #include <embedded/drivers/include/serial.h>
 #include <embedded/drivers/src/serial.cpp>
 #define T_MESSAGE_MS 1000  // 1 second 
@@ -26,8 +28,30 @@ controlsWrapper::controlsWrapper(QByteArray &bytes, QMutex &mutex, std::atomic<b
 // This is the firmware main loop. It's called in a separate thread in DataUnpacker.cpp
 // Put your testing code here!
 void controlsWrapper::startThread() {
+    // initialize tca
+    Tca6416 tca = Tca6416(0, 0x20);
+    // set enable signals P05, P06, P14, P15, P16 to write (0)
+    uint8_t directions[16]= {1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1};
+    device.begin(directions);
+    // initialize vars for input toggle signals
+    uint8_t lblnk_toggle = 0;
+    uint8_t rblnk_toggle = 0;
+    uint8_t hl_toggle = 0;
+
     int messages_not_received = 0;
     while(true) {
+        // TCA code
+        // read input signals
+        lblnk_toggle = tca.get_state(0, 0);
+        rblnk_toggle = tca.get_state(0, 1);
+        hl_toggle = tca.get_state(0, 4);
+        // write to enable signals
+        tca.set_state(1, 4, lblnk_toggle); // FL_TS_LED_EN
+        tca.set_state(1, 5, rblnk_toggle); // FR_TS_LED_EN
+        tca.set_state(1, 6, hl_toggle); // F_HL_LED_EN
+
+
+        // UART code
         std::cout << "===========================================" << std::endl;
         char buffTemp[TOTAL_BYTES];
         // read
@@ -64,7 +88,6 @@ void controlsWrapper::startThread() {
         bytes = QByteArray::fromRawData(buffTemp, TOTAL_BYTES);
         mutex.unlock();
         //usleep(1000000);
-        //std::cout << "==========================================" << std::endl;
         sleep(1);
     }
 }
