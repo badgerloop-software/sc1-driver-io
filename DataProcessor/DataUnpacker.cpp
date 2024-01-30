a867c8e34339d61ebb32280d6f04126961fc3468
Mingcan Li <mli727@wisc.edu>
2024-01-30 15:10:21 -0600

    change dataformat directory to reflect change in build directory

--- DataProcessor/DataUnpacker.cpp
+++ DataProcessor/DataUnpacker.cpp
@@ -40,7 +40,7 @@ DataUnpacker::DataUnpacker(QObject *parent) : QObject(parent)
 {
     FILE* fp = fopen("../sc1-driver-io/sc1-data-format/format.json", "r"); // NOTE: Windows: "rb"; non-Windows: "r"
     if(fp == 0) {
-        fp = fopen("../solar-car-dashboard/sc1-data-format/format.json", "r"); // NOTE: Windows: "rb"; non-Windows: "r"
+        fp = fopen("../sc1-data-format/format.json", "r"); // NOTE: Windows: "rb"; non-Windows: "r"
     }
 
     char readBuffer[65536];
